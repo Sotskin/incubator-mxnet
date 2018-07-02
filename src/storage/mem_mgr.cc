445b4f816c058dfc01e255162417f848371270bb
Sotskin <sotskin@gmail.com>
2018-07-02 19:12:34 +0000

    naive mem mgr

--- src/storage/mem_mgr.cc
+++ src/storage/mem_mgr.cc
@@ -2,9 +2,11 @@
 #include <cuda_runtime_api.h>
 #include <math.h>
 #include <mxnet/mem_mgr.h>
-#include <../common/cuda_utils.h>
+#include "../common/cuda_utils.h"
 
 namespace mxnet {
+
+const size_t hack_size = 0;
 
 MemoryManager* MemoryManager::Get() {
   static MemoryManager* mm = _GetSharedRef().get();
@@ -17,24 +19,27 @@ std::shared_ptr<MemoryManager> MemoryManager::_GetSharedRef() {
 }
 
 MemoryManager::MemoryManager() {
+  std::cout << "Initialize Memory Allocator" << std::endl;
 }
 
-MemoryManger::~MemoryManger() {
+MemoryManager::~MemoryManager() {
+  std::cout << "Destroy Memory Allocator" << std::endl;
 }
 
-cudaError_t MemoryManger::Malloc(void*& devptr, size_t size, int device_id){
-
-  return cudaSuccess;
+cudaError_t MemoryManager::Malloc(void*& devptr, size_t size, int device_id){
+  cudaSetDevice(device_id);
+  return cudaMalloc(&devptr, size);
 }
 
-cudaError_t MemroyManger::Free(void* devptr, int device_id){
-
-  return cudaSuccess;
+cudaError_t MemoryManager::Free(void* devptr, int device_id){
+  cudaSetDevice(device_id);
+  return cudaFree(devptr);
 }
 
-cudaError_t MemoryManger::Memcpy(int device_id, void* dst, const void* src,
-    size_t, count, enum cudaMemcpyKind kind) {
-  return cudaSuccess;
+cudaError_t MemoryManager::Memcpy(int device_id, void* dst, const void* src,
+    size_t count, enum cudaMemcpyKind kind) {
+  cudaSetDevice(device_id);
+  return cudaMemcpy(dst, src, count, kind);
 }
 
 cudaError_t MemoryManager::MemGetInfo(int device_id, size_t *total, 
@@ -44,7 +49,10 @@ cudaError_t MemoryManager::MemGetInfo(int device_id, size_t *total,
 
 
 bool MemoryManager::TryAllocate(int device_id, size_t size) {
-  return true;
+  cudaSetDevice(device_id);
+  size_t free, total;
+  cudaMemGetInfo(&free, &total);
+  return free > size + hack_size;
 }
 
 } // namespace mxnet

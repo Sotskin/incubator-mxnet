4fc035de279fb3e2cb00fd1cea868b9a2b32d89f
Sotskin <sotskin@gmail.com>
2018-07-17 20:29:26 +0000

    fix

--- benchmark_tests/benchmark.py
+++ benchmark_tests/benchmark.py
@@ -62,10 +62,9 @@ def test():
                         grad_req='write',
                         group2ctx=group2ctx)
     feed_args(net, arg_arrays)
-    print('bind ends')
     import time
     time.sleep(10)
-    print('bind 1')
+    print('Bind End')
     all_time = []
     t0 = time.time()
     for i in range(num_loops):

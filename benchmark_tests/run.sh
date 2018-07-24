5286ecd9fdc42ccf89c4f34e4433ce5799995076
qingsen <qingsen@geeker-3.news.cs.nyu.edu>
2018-07-24 17:45:49 +0000

    change run.sh

--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -1,4 +1,9 @@
-LAYERS=18
-WIDE_SCALE=8
+LAYERS=269
+WIDE_SCALE=4
+
+export CUDA_VISIBLE_DEVICES=0
+export MXNET_ENGINE_TYPE=NaiveEngine
+export PYTHONPATH=/home/qingsen/incubator-mxnet/python
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 
 python benchmark.py --num_layers=${LAYERS} --batch_size 4 --wide_scale=${WIDE_SCALE} --num_loop=6 resnet > log_resnet_${LAYERS}_4_${WIDE_SCALE}

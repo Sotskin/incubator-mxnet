43650263ecfb88fbb5b2add8816eb4b5750ab5cc
qingsen <qingsen@geeker-3.news.cs.nyu.edu>
2018-07-24 18:53:02 +0000

    new run.sh

--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -1,7 +1,6 @@
-LAYERS=$269
-BATCH_SIZE=$4
-WIDE_SCALE=$4
->>>>>>> b82697de72fb71b9efc76944d126d5583ff1122a
+LAYERS=269
+BATCH_SIZE=4
+WIDE_SCALE=4
 
 echo "LAYERS = ${LAYERS}"
 echo "BATCH_SIZE = ${BATCH_SIZE}"
@@ -9,8 +8,8 @@ echo "WIDE_SCALE = ${WIDE_SCALE}"
 
 export SWAP_ALGORITHM=NaiveHistory
 export MXNET_ENGINE_TYPE=NaiveEngine
-export PYTHONPATH=/home/sotskin/incubator-mxnet/python
+export PYTHONPATH=/home/qingsen/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=6 resnet > log_resnet_${LAYERS}_4_${WIDE_SCALE}

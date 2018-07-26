d99109669dc5142270d9aba2b10e9268e3e61c26
Sotskin <sotskin@gmail.com>
2018-07-26 18:25:15 +0000

    move sizehistory algorithm

--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -8,10 +8,10 @@ echo "BATCH_SIZE = ${BATCH_SIZE}"
 echo "WIDE_SCALE = ${WIDE_SCALE}"
 
 export PREFETCH_STEP_AHEAD=50
-export SWAP_ALGORITHM=NaiveHistory
+export SWAP_ALGORITHM=LRU
 export MXNET_ENGINE_TYPE=NaiveEngine
-export PYTHONPATH=/home/karl/incubator-mxnet/python
+export PYTHONPATH=/home/sotskin/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
-python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=6 resnet > log_resnet_${LAYERS}_4_${WIDE_SCALE}
+python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=12 resnet > log_resnet_${LAYERS}_4_${WIDE_SCALE}

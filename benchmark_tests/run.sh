e16e1c0c221e315d7562ffdf2e3bb0ee275a4300
fegin <fegins@gmail.com>
2018-07-31 13:29:45 -0400

    merge

--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -14,9 +14,9 @@ export MXNET_PREFETCH_STEP_AHEAD=${STEPS}
 export MXNET_PREFETCH_ALGORITHM=${PREFETCH}
 export MXNET_SWAP_ALGORITHM=${SWAP}
 export MXNET_ENGINE_TYPE=NaiveEngine
-export PYTHONPATH=/home/qingsen/incubator-mxnet/python
+export PYTHONPATH=/home/sotskin/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=10 resnet > log_resnet_${LAYERS}_${BATCH_SIZE}_${WIDE_SCALE}_${SWAP}_${PREFETCH}_${STEPS}
 
